30f27050ac1c95610fece8cbcad141255382e1e8
Irene Scott <irenejinscott@gmail.com>
2020-06-10 14:32:08 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,16 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
-
+#this file is requireing the gems in our gemfile and giving our program access to them
 
 require 'bundler/setup'
 Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+:adapter => "sqlite3",
+:database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
